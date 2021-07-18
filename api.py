a82c92b8a6f428dc1679e8cd90f2064870ac59b0
IndraP24 <indrashis985@gmail.com>
2021-07-18 13:18:46 +0530

    Remove main method

--- api.py
+++ api.py
@@ -94,6 +94,3 @@ async def predict_image(file: UploadFile = File(...)):
         e = sys.exc_info()[1]
         raise HTTPException(status_code=500, detail=str(e))
 
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, debug=True)
